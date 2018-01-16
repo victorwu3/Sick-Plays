7518b52d72b0e3f7f85b2f096425ad637765d4a5
Victor Wu <victor.wu.1@vanderbilt.edu>
2018-01-15 20:13:03 -0500

    settings

--- src/sickplays/settings.py
+++ src/sickplays/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'djreact/static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'djreact/static'),
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'djreact/static')
